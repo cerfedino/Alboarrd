838a4ea53cbcf32ae525bd7c188bf6998cc877ea
Albert Cerfeda <cerfeda.albert@crfda.com>
2025-11-13 23:08:32 +0000

    feat: Added link card rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,47 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { getAuthContext } from "@/lib/auth-headers";
+import { promises as fs } from "fs";
+import path from "path";
+import YAML from "yaml";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card } from "@/components/ui/card";
 
 export default async function Home() {
-  const { user, groups } = await getAuthContext();
+  const { user, groups, real_name } = await getAuthContext();
   if (!user) return <h1>Not authenticated</h1>;
 
+  const filePath = path.join("/etc/config.yaml");
+  const fileContents = await fs.readFile(filePath, "utf8");
+  const LINKS = YAML.parse(fileContents); // JS object
+
   return (
-    <div className="min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello {user}</h1>
-      <p>{groups.join(", ")}</p>
+    <div className="max-h-screen pa-4 items-center justify-center font-sans">
+      <ThemeToggle />
+      <h1 className="text-4xl w-fill text-center">
+        Hello <b>{real_name ? real_name : user}</b>
+      </h1>
+      <p className="text-xl w-fill text-center">Service dashboard</p>
+      <div className="flex max-w-screen flex-row flex-wrap gap-6 items-center justify-center mt-14 mb-4">
+        {LINKS.links
+          .filter(
+            (link: any) =>
+              !link.groups ||
+              link.groups.some((r: string) => groups.includes(r))
+          )
+          .map((link: any) => (
+            <a key={link.name} href={link.url}>
+              <Card className="size-40 flex items-center justify-center gap-0 p-4 hover:shadow-lg transition-shadow hover:bg-accent/50 group">
+                <p className="text-xl align-center">{link.name}</p>
+                <Avatar className="size-16 mt-4">
+                  <AvatarImage src={link.icon} alt={link.name} />
+                  <AvatarFallback className="text-xl">
+                    {link.name.substring(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+              </Card>
+            </a>
+          ))}
+      </div>
     </div>
   );
 }
--- src/lib/auth-headers.ts
+++ src/lib/auth-headers.ts
@@ -3,14 +3,16 @@ import { headers } from "next/headers";
 export type AuthContext = {
   user: string | null;
   groups: string[];
+  real_name: string | null;
 };
 
 export async function getAuthContext(): Promise<AuthContext> {
   const hdrs = await headers();
   const user = hdrs.get("remote-user");
+  const real_name = hdrs.get("remote-name");
   const groupsHeader = hdrs.get("remote-groups");
   const groups = groupsHeader
     ? groupsHeader.split(",").map((g) => g.trim())
     : [];
-  return { user, groups };
+  return { user, groups, real_name };
 }
